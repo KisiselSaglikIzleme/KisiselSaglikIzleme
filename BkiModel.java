4757c3d88dd1f9f4074f320ca140039e8cc6baf7
Recep Batuhan GÜNAY <58995809+BatuhanGunay@users.noreply.github.com>
2020-06-02 07:54:28 +0300

    BkiModel.java

---
+++ BkiModel.java
@@ -0,0 +1,13 @@
+package com.example.saglik;
+
+public class BkiModel {
+    private String bki;
+    public BkiModel(String bki){
+        this.bki = bki;
+    }
+    public String getBki(){return bki;
+    }
+    public void setBki(String bki){
+        this.bki= bki;
+    }
+}
