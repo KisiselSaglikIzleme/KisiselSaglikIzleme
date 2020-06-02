b8a14052e91b7d7175d51aba9d0339db0491daef
Recep Batuhan GÜNAY <58995809+BatuhanGunay@users.noreply.github.com>
2020-06-02 07:53:40 +0300

    BildirimYakalıyıcı.java

---
+++ BildirimYakalayici.java
@@ -0,0 +1,19 @@
+package com.example.saglik;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BildirimYakalayıcı extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager bildirimYoneticisi = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification bildirim =intent.getParcelableExtra("nesne");
+        bildirimYoneticisi.notify(1,bildirim);
+
+
+    }
+}
