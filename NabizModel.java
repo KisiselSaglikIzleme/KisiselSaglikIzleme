d6aeda75e17aeab02ff6dba18f6adc8295bdfdd8
Recep Batuhan GÜNAY <58995809+BatuhanGunay@users.noreply.github.com>
2020-06-21 16:09:08 +0300

    NabizModel.java
    oluşturuldu

---
+++ NabizModel.java
@@ -0,0 +1,14 @@
+package com.example.saglik;
+
+public class NabizModel {
+    private String nabiz;
+    public NabizModel(String nabiz){
+        this.nabiz = nabiz;
+    }
+    public String getNabiz(){
+        return nabiz;
+    }
+    public void setNabiz(String nabiz){
+        this.nabiz = nabiz;
+    }
+}
